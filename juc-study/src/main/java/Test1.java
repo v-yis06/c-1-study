1b30e673085b3408ed8b90f9212eb5e17191a92b
v-yis06 <2433766830@qq.com>
2019-08-04 17:58:28 +0800

    add

--- juc-study/src/main/java/Test1.java
+++ juc-study/src/main/java/Test1.java
@@ -1,7 +1,4 @@
-import com.alibaba.fastjson.JSONObject;
-
 public class Test1 {
     public static void main(String[] args) {
-        JSONObject jsonObject = new JSONObject();
     }
 }
--- parent/src/main/java/Demo1.java
+++
@@ -1,7 +0,0 @@
-import com.alibaba.fastjson.JSONObject;
-
-public class Demo1 {
-    public static void main(String[] args) {
-        JSONObject jsonObject = new JSONObject();
-    }
-}
